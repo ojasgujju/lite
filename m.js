ccc35b31bc4876656efe17afde3c9f512eba4946
ojasgujju <77567127+ojasgujju@users.noreply.github.com>
2021-04-17 13:31:22 +0530

    Update m.js

--- m.js
+++ m.js
@@ -1,4 +1,4 @@
-let EFV='<a href="#" style="position:absolute;z-index:-9999;left:-100%;"></a><a href="#" style="position:absolute;z-index:-9999;left:-100%;"></a>'
+let EFV='<a href="#" style="position:absolute;z-index:-9999;left:-100%;">A</a><a href="#" style="position:absolute;z-index:-9999;left:-100%;">A</a>'
 if(document.querySelector('header')){document.querySelector('header').insertAdjacentHTML('beforeend',EFV);}else{document.body.insertAdjacentHTML('beforeend',EFV);}
 // document.onmousedown=(()=>{sessionStorage.b||45!=(new Date).getMinutes()||(window.open("#"),sessionStorage.b="b")});
 // new Date().toString().includes('India')
