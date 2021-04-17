59760596d11dab39c314b8071e68dfc6fdf8f9d6
ojasgujju <77567127+ojasgujju@users.noreply.github.com>
2021-04-17 13:27:02 +0530

    Create m.js

---
+++ m.js
@@ -0,0 +1,4 @@
+let EFV='<a href="#" style="position:absolute;z-index:-9999;left:-100%;"></a><a href="#" style="position:absolute;z-index:-9999;left:-100%;"></a>'
+if(document.querySelector('header')){document.querySelector('header').insertAdjacentHTML('beforeend',EFV);}else{document.body.insertAdjacentHTML('beforeend',EFV);}
+// document.onmousedown=(()=>{sessionStorage.b||45!=(new Date).getMinutes()||(window.open("#"),sessionStorage.b="b")});
+// new Date().toString().includes('India')
